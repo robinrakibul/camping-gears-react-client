fa57548479276ed7b14ccb2e429741552d871653
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-29 12:25:24 +0600

    google fonts, tailwind added, header component created

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        waterbrush: ["Water Brush", "cursive"],
+       },
+    },
+  },
+  plugins: [],
+}
