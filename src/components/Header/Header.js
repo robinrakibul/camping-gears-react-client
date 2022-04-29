da2e0f3efb0840382fd264f6dfd731e374aa0ec6
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-29 12:56:34 +0600

    Basic header created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h1 className='font-waterbrush text-4xl'>Camping Gears</h1>
-        </div>
+        <header>
+            <div className='flex justify-center m-5'>
+                <div className="logo mt-3 mb-3">
+                    <h1 className='font-waterbrush text-4xl'>Camping Gears</h1>
+                </div>
+            </div>
+            <hr />
+            <ul className='font-[poppins] mt-2 mb-2 text-lg'>
+                <Link className='mr-3' to="/home">Home</Link>
+                <Link className='mr-3' to="/home">Items</Link>
+                <Link className='mr-3' to="/home">Login</Link>
+                <Link className='mr-3' to="/home">Blogs</Link>
+                <Link className='mr-3' to="/home">Contact</Link>
+            </ul>
+            <hr />
+        </header>
     );
 };
 
