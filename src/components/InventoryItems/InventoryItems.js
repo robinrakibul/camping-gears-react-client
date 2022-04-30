5bb94c2e29dccba9fa0d852b12089f907ed602df
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-30 14:03:29 +0600

    changed in api, updated at mongodb, showing in items section

--- src/components/InventoryItems/InventoryItems.js
+++ src/components/InventoryItems/InventoryItems.js
@@ -9,8 +9,8 @@ const InventoryItems = () => {
     }, [])
     return (
         <div>
-            <h2 className='text-white font-[Poppins] text-2xl font-light border-b-2 ml-64 mr-64 mb-10'>Inventory Items [SHOW 6 ITEMS MAX]</h2>
-            <div>
+            <h2 className='text-white font-[Poppins] text-2xl font-light border-b-2 ml-5 mr-5 md:ml-64 md:mr-64 mb-10'>Inventory Items [SHOW 6 ITEMS MAX]</h2>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-2'>
                 {
                     items.map(item =>
                         <SingleItem key={item._id} item={item}>
--- src/components/InventoryItems/SingleItem.js
+++ src/components/InventoryItems/SingleItem.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const SingleItem = ({item}) => {
+const SingleItem = ({ item }) => {
+    const { title, image, price, description, supplier, quantity} = item;
     return (
         <div>
-            <h1>{item._id}</h1>
+            <div class="flex justify-center mb-5">
+                <div class="rounded-lg shadow-lg bg-white max-w-sm w-72">
+                    <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                    <img width='300px' height='auto' class="rounded-t-lg object-cover" src={image} alt="" />
+                    </a>
+                    <p className='text-orange-500 font-bold'>Price: {price}</p>
+                    <small>Supplier: {supplier}</small>
+                    <p>Available Quantity: {quantity}</p>
+                    <div class="p-6">
+                        <h5 class="text-gray-900 text-xl font-medium mb-2">{title}</h5>
+                        <p class="text-gray-700 text-base mb-4">
+                           {description.slice(0,50)}...
+                        </p>
+                        <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
