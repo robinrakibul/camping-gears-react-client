1faf88f1b405b926103ba910a6cc893a1b781c12
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-30 14:13:23 +0600

    sliced items in home page

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
             <div className='mt-16'>
                 <p className='text-xl text-[#fe4a49]'>Track your sales now!</p>
                 <h1 className='font-[librebodoni] text-5xl md:text-7xl font-bold mb-5'>Keep track of <br /> your camping inventory</h1>
-                <button className='pt-3 pb-3 pl-4 pr-4 rounded bg-orange-600 mb-5'>Start Now</button>
+                <button className='pt-3 pb-3 pl-4 pr-4 rounded bg-orange-600 hover:bg-orange-700 mb-5' data-mdb-ripple="true" data-mdb-ripple-color="light">Start Now</button>
             </div>
             <div>
                 <img className='rounded m-2 ml-5 md:ml-0' width='650px' src={campimage} alt="" />
--- src/components/InventoryItems/InventoryItems.js
+++ src/components/InventoryItems/InventoryItems.js
@@ -9,13 +9,20 @@ const InventoryItems = () => {
     }, [])
     return (
         <div>
-            <h2 className='text-white font-[Poppins] text-2xl font-light border-b-2 ml-5 mr-5 md:ml-64 md:mr-64 mb-10'>Inventory Items [SHOW 6 ITEMS MAX]</h2>
+            <h2 className='text-white font-[Poppins] text-2xl font-light border-b-2 ml-5 mr-5 md:ml-64 md:mr-64 mb-10'>Inventory Items</h2>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-2'>
                 {
-                    items.map(item =>
+                    items.slice(0, 6).map(item =>
                         <SingleItem key={item._id} item={item}>
                         </SingleItem>)
                 }
+            </div>
+            <div className="flex justify-center align-middle mt-5 mb-4">
+                <button className='w-72 pt-2 pb-2 pl-4 pr-3 rounded bg-orange-600 hover:bg-orange-700 mb-5 text-white' data-mdb-ripple="true" data-mdb-ripple-color="light">Check Inventory 
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-7 ml-2 inline" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clip-rule="evenodd" />
+                </svg>
+                </button>
             </div>
         </div>
     );
