2d3d514e7ed685c212b6ecd4040d2e4e565d229a
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-30 15:22:56 +0600

    fix issues with slice in homepage and the inventory page

--- src/components/InventoryItems/InventoryItems.js
+++ src/components/InventoryItems/InventoryItems.js
@@ -12,7 +12,7 @@ const InventoryItems = () => {
             <h2 className='text-white font-[Poppins] text-2xl font-light border-b-2 ml-5 mr-5 md:ml-64 md:mr-64 mb-10'>Inventory Items</h2>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-2'>
                 {
-                    items.slice(0, 6).map(item =>
+                    items.map(item =>
                         <SingleItem key={item._id} item={item}>
                         </SingleItem>)
                 }
