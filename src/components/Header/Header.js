1784c2a9730fc2ba7689f3ae491f00f7920d6d93
robinrakibul <robinrakibulhassan@gmail.com>
2022-05-01 10:46:44 +0600

    responsive navbar done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import { faBars,faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './header.css';
 const Header = () => {
+    const [open, setOpen] = useState(false);
     return (
-        <header className="bg-[#001730] text-white">
+        <header className="bg-[#001730] text-white md:h-52">
             <div className='flex justify-center'>
                 <div className="logo mt-3 mb-3">
-                    <Link to="/"><h1 className='font-waterbrush p-5 text-3xl md:text-5xl'>Camping Gears</h1></Link>
+                    <Link to="/"><h1 className='font-waterbrush p-2 md:p-5 text-3xl md:text-5xl'>Camping Gears</h1></Link>
                 </div>
             </div>
-            <ul className='font-[poppins] pt-3 pb-3 text-xl font-medium border-t-2 border-b-2 border-white'>
-                <Link className='ml-5 mr-5 hover:text-[#fe4a49] duration-300' to="/">Home</Link>
-                <Link className='mr-5 hover:text-[#fe4a49] duration-300' to="/items">Inventory</Link>
-                <Link className='mr-5 hover:text-[#fe4a49] duration-300' to="/login">Login</Link>
-                <Link className='mr-5 hover:text-[#fe4a49] duration-300' to="/blogs">Blogs</Link>
-                <Link className='mr-5 hover:text-[#fe4a49] duration-300' to="/contact">Contact</Link>
+            <div className='text-2xl absolute top-6 right-5 pb-2 cursor-pointer md:hidden' onClick={()=>setOpen(!open)}>
+                <FontAwesomeIcon icon={open ? faBars : faXmark }></FontAwesomeIcon>
+            </div>
+            <ul className={`font-[poppins] md:pt-3 md:pb-3 md:mb-0 text-xl font-medium md:border-t-2 md:border-b-2 md:border-white
+            flex flex-col md:flex-row md:justify-center absolute md:static md:z-auto z[-1] bg-[#001730] w-full md:w-auto transition-all duration-500 ease-in ${open ? 'top-20 opacity-100' : 'top-[-490px] opacity-0 md:opacity-100'}`}>
+                <Link className='md:ml-5 mr-5 mb-5 md:mb-0 hover:text-[#fe4a49] duration-300' to="/">Home</Link>
+                <Link className='mr-5 mb-5 md:mb-0 hover:text-[#fe4a49] duration-300' to="/items">Inventory</Link>
+                <Link className='mr-5 mb-5 md:mb-0 hover:text-[#fe4a49] duration-300' to="/login">Login</Link>
+                <Link className='mr-5 mb-5 md:mb-0 hover:text-[#fe4a49] duration-300' to="/blogs">Blogs</Link>
+                <Link className='mr-5 mb-5 md:mb-0 hover:text-[#fe4a49] duration-300' to="/contact">Contact</Link>
             </ul>
         </header>
     );
