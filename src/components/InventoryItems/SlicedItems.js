e803fd3213af9ed5eef9d6dc98b70581f9100f31
robinrakibul <robinrakibulhassan@gmail.com>
2022-05-01 16:29:53 +0600

    navigate to all items while manage inventories clicked

--- src/components/InventoryItems/SlicedItems.js
+++ src/components/InventoryItems/SlicedItems.js
@@ -2,14 +2,20 @@ import React, { useEffect, useState } from 'react';
 import SingleItem from '../InventoryItems/SingleItem';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 const SlicedItems = () => {
     const [items, setitems] = useState([]);
+    let navigate = useNavigate();
     useEffect(() => {
         fetch('https://warehouse-management-server.herokuapp.com/items')
             .then(res => res.json())
             .then(data => setitems(data))
-    }, [])
+    }, []);
+
+    const navigateInventories = () =>{
+        navigate('/items');
+    }
     return (
         <div>
             <h2 className='text-white font-[Poppins] text-2xl font-light border-b-2 ml-5 mr-5 md:ml-64 md:mr-64 mb-10'>Inventory Items</h2>
@@ -21,7 +27,7 @@ const SlicedItems = () => {
                 }
             </div>
             <div className='flex justify-center'>
-                <button className='pt-3 pb-3 pl-4 pr-4 text-white rounded bg-orange-600 hover:bg-orange-700 mb-5' data-mdb-ripple="true" data-mdb-ripple-color="light">Manage Inventories
+                <button onClick={navigateInventories} className='pt-3 pb-3 pl-4 pr-4 text-white rounded bg-orange-600 hover:bg-orange-700 mb-5' data-mdb-ripple="true" data-mdb-ripple-color="light">Manage Inventories
                     <FontAwesomeIcon className='pl-2 text-white' icon={faArrowRight} />
                 </button>
             </div>
