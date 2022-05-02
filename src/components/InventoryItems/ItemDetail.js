a2412887ac47f3c41161b238f341fbaab02346da
robinrakibul <robinrakibulhassan@gmail.com>
2022-05-02 14:06:52 +0600

    fixed consolelog errors

--- src/components/InventoryItems/ItemDetail.js
+++ src/components/InventoryItems/ItemDetail.js
@@ -26,18 +26,18 @@ const ItemDetail = () => {
         <div className='text-white flex flex-col items-center'>
 
             {/* Flowbite Card */}
-            <div class="max-w-sm bg-white text-red rounded-lg shadow-md mt-5 font-[poppins]">
-                <h1 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-3 mb-3">Stock Details:</h1>
-                <h5 class="text-lg tracking-tight text-gray-900 dark:text-white"><span className='font-semibold'>Product Id: </span>{item._id}</h5>
-                <h5 class="text-lg tracking-tight text-gray-900 dark:text-white"><span className='font-semibold'>Product Name: </span>{item.title}</h5>
-                <h5 class="text-lg tracking-tight text-gray-900 dark:text-white"><span className='font-semibold'>Supplier Name: </span>{item.supplier}</h5>
-                <img class="p-4 rounded-3xl" src={item.image} alt="" />
-                <h5 class="text-lg tracking-tight text-gray-900 dark:text-white"><span className='font-semibold'>Description: </span>{item.description}</h5>
-                <h5 class="text-xl border-t-2 border-b-2 border-orange-400 tracking-tight text-gray-900 dark:text-white">Quantity: <span className='font-bold'>{item.quantity}</span></h5>
-                <div class="px-5 pt-5 pb-5">
-                    <div class="flex justify-between items-center">
-                        <span class="text-3xl font-bold text-gray-900 dark:text-white">{item.price} Tk</span>
-                        <Link to="" class="text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-yellow-500 dark:hover:bg-yellow-600 dark:focus:ring-yellow-800">Delivered</Link>
+            <div className="max-w-sm bg-white text-red rounded-lg shadow-md mt-5 font-[poppins]">
+                <h1 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white mt-3 mb-3">Stock Details:</h1>
+                <h5 className="text-lg tracking-tight text-gray-900 dark:text-white"><span className='font-semibold'>Product Id: </span>{item._id}</h5>
+                <h5 className="text-lg tracking-tight text-gray-900 dark:text-white"><span className='font-semibold'>Product Name: </span>{item.title}</h5>
+                <h5 className="text-lg tracking-tight text-gray-900 dark:text-white"><span className='font-semibold'>Supplier Name: </span>{item.supplier}</h5>
+                <img className="p-4 rounded-3xl" src={item.image} alt="" />
+                <h5 className="text-lg tracking-tight text-gray-900 dark:text-white"><span className='font-semibold'>Description: </span>{item.description}</h5>
+                <h5 className="text-xl border-t-2 border-b-2 border-orange-400 tracking-tight text-gray-900 dark:text-white">Quantity: <span className='font-bold'>{item.quantity}</span></h5>
+                <div className="px-5 pt-5 pb-5">
+                    <div className="flex justify-between items-center">
+                        <span className="text-3xl font-bold text-gray-900 dark:text-white">{item.price} Tk</span>
+                        <Link to="" className="text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-yellow-500 dark:hover:bg-yellow-600 dark:focus:ring-yellow-800">Delivered</Link>
                     </div>
                 </div>
             </div>
